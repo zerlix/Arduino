50c8c316a662dc7587f64755b3a4ee302e96e889
Torsten Hemm <t.hemm@gmx.de>
2023-08-31 18:05:08 +0200

    fixes

--- Software_SPI/Software_SPI.ino
+++ Software_SPI/Software_SPI.ino
@@ -1,5 +1,11 @@
 #include <SPI.h>             
-#include <SdFat.h>           
+#include <SdFat.h>
+
+// Software SPI muss in der Datei ..\Arduino\libraries\SdFat\src\SdFatConfig.h aktiviert werden
+// dazu muss SPI_DRIVER_SELECT auf 2 gesetzt werden.
+#if SPI_DRIVER_SELECT != 2
+#error  SPI_DRIVER_SELECT != 2
+#endif
 
 // SPI Pins 
 const uint8_t SOFT_MISO_PIN = 12;
@@ -9,17 +15,12 @@ const uint8_t SOFT_SCK_PIN = 13;
 // Chip select 
 const uint8_t SD_CS_PIN = 10; // SD Karte
 
-// Check SdFatConfig.h, ob Software SPI aktiviert wurde 
-#if SPI_DRIVER_SELECT != 2
-#error  SPI_DRIVER_SELECT muss auf 2 stehen- SdFatConfig.h 
-#endif
-
 // Chipselect Pins
 #define SD_CS SdSpiConfig(SD_CS_PIN, DEDICATED_SPI, SD_SCK_MHZ(0), &softSpi) // Sd Karten Slot
 
 SoftSpiDriver<SOFT_MISO_PIN, SOFT_MOSI_PIN, SOFT_SCK_PIN> softSpi; //Pin belegung vom SD Karten Slot 
 
-SdFat SD;
+SdFat sd;
 FsFile file;
 
 void setup() {
@@ -27,23 +28,28 @@ void setup() {
     Serial.begin(9600);
 
 
-
-    if (!SD.begin(SD_CS)) {
-      SD.initErrorHalt();
+    if (!sd.begin(SD_CS)) {
+      sd.initErrorHalt();
     }
 
+    
+    // Datei öffnen
     if (!file.open("SoftSPI.txt", O_RDWR | O_CREAT)) {
-      SD.errorHalt(F("open failed"));
+      sd.errorHalt(F("open failed"));
     }
 
+    // Datei ausgeben
     while (file.available()) {
       Serial.write(file.read());
     }
 
+    // Datei schliessen
     file.close();
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
