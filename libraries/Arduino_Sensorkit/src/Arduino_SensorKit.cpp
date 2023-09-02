5effac550e40c9ea1b7e3e8c517a72130d389de0
Torsten Hemm <t.hemm@gmx.de>
2023-09-02 11:18:50 +0200

    .

--- libraries/Arduino_Sensorkit/src/Arduino_SensorKit.cpp
+++
@@ -1,8 +0,0 @@
-#include "Arduino_SensorKit.h"
-
-//Declare component's classes
-U8X8_SSD1306_128X64_NONAME_SW_I2C Oled(/* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
-DHT Environment(DHTPIN,DHT11);
-DHT Environment_I2C(DHT20);
-SensorKit_LIS3DHTR Accelerometer;
-SensorKit_BMP280 Pressure;
