5bb49e0241448ed8d935f2d7f34a676d99e77360
Torsten Hemm <t.hemm@gmx.de>
2023-09-18 16:53:07 +0200

    shell.h und shell.cpp zusamengeführt

--- SpuFirm/logger.h
+++ SpuFirm/logger.h
@@ -9,16 +9,17 @@
 // SPI Pins (SD Card)
 const uint8_t SOFT_MISO_PIN = 12;
 const uint8_t SOFT_MOSI_PIN = 11;
-const uint8_t SOFT_SCK_PIN  = 13;
+const uint8_t SOFT_SCK_PIN = 13;
 // Chip select Pin (SD Card)
-const uint8_t SD_CS_PIN     = 10;  
+const uint8_t SD_CS_PIN = 10;
 
 // Chipselect Pin Macro für SD Karten Slot definieren
 #define SD_CS SdSpiConfig(SD_CS_PIN, DEDICATED_SPI, SD_SCK_MHZ(0), &softSpi)
 
+// Deklarationen in SpuFirm.ino
+extern SdFat sd;
 
 
-extern SdFat sd; 
 
 /**
  * @class Logger
@@ -36,48 +37,47 @@ private:
 
 public:
 
-/**
+
+  /**
  * Konstruktor Methode wird automatisch aufgerufen
  * wenn ein Object von der Klasse "Logger" erstellt wird
  **/
-Logger::Logger() 
-{
-
-}
+  Logger::Logger() {
+  }
 
 
 
-/** 
+  /** 
  * Schreibt eine Zeile in die logdatei
  * \param[in] str zeile die in die datei geschrieben werden soll
  * \return true|false
  **/
-bool Logger::writeLog(const char* str) {
-  // Öffne des Logfiles
-  if (!logFile.open(logFileName, O_RDWR | O_CREAT | O_APPEND)) {
-    Serial.println(F("Logger:writeLog() - Kann Datei nicht öffnen"));
-    return false;  // funktion beenden und false zurückgeben
+  bool Logger::writeLog(const char* str) {
+    // Öffne des Logfiles
+    if (!logFile.open(logFileName, O_RDWR | O_CREAT | O_APPEND)) {
+      Serial.println(F("Logger:writeLog() - Kann Datei nicht öffnen"));
+      return false;  // funktion beenden und false zurückgeben
+    }
+
+    // Schreibe die daten in die datei
+    logFile.write(str);
+    logFile.write('\n');  // Zeilenumbruch (newline)
+
+    // Sicherstellen das der Buffer geleert und die datei vollständig geschrieben wurde
+    logFile.sync();
+
+    // Falls Fehler aufgetreten sind. Meldung auf der Konsole ausgeben
+    if (logFile.getWriteError()) {
+      Serial.println(F("Logger::write() - Kann die Datei nicht schreiben"));
+      return false;  // funktion beenden und false zurückgeben
+    }
+
+    // Datei schliessen um Speicherplatz freizugeben
+    logFile.close();
+
+    // Wenn wir hier angekommen sind hat alles geklappt und geben ein true zurück
+    return true;
   }
-
-  // Schreibe die daten in die datei
-  logFile.write(str);
-  logFile.write('\n'); // Zeilenumbruch (newline)
-  
-  // Sicherstellen das der Buffer geleert und die datei vollständig geschrieben wurde
-  logFile.sync();
-
-  // Falls Fehler aufgetreten sind. Meldung auf der Konsole ausgeben
-  if (logFile.getWriteError()) {
-    Serial.println(F("Logger::write() - Kann die Datei nicht schreiben"));
-    return false;  // funktion beenden und false zurückgeben
-  }
-
-  // Datei schliessen um Speicherplatz freizugeben
-  logFile.close();
-
-  // Wenn wir hier angekommen sind hat alles geklappt und geben ein true zurück
-  return true;
-}
 };
 
 #endif
--- SpuFirm/shell.h
+++ SpuFirm/shell.h
@@ -3,10 +3,13 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <SdFat.h>
+#include "HardwareSerial.h"
+#include "shell.h"
 #include "sensors.h"
 
+// Deklarationen in SpuFirm.ino
 extern SdFat sd;
-extern Sensors s;   
+extern Sensors s;
 
 /**
  * @class Shell
@@ -14,22 +17,96 @@ extern Sensors s;
  * TODO:
  *     - command parser für argumente
  **/
-class Shell
-{
+class Shell {
 
-  public:
-    Shell::Shell();  
-    bool Shell::getCommand(); // überprüft Serielle konsole auf eingabe
+private:
+  String sBuffer;  // Buffer für input
 
 
-  private:
-    String sBuffer; // Buffer für input
+public:
 
-    bool Shell::_execCommand(); // führt commando (sBuffer) aus
-    void Shell::_help(); // commando help
-    void Shell::_free(); // commando free
-    void Shell::_ls();  // commando ls
-    void Shell::_sensors(); // zeigt sensor daten an
-    
+
+
+  Shell::Shell() {
+  }
+
+
+
+  /**
+ * liest commando von seriellen console,
+ * speichert es in sBuffer und ruft _execCommand() auf
+ */
+  bool Shell::getCommand() {
+
+    if (Serial.available() > 0) {
+      sBuffer = Serial.readStringUntil('\n');
+      _execCommand();
+    }
+    return true;
+  }
+
+
+private:
+/**
+ * führt Methode entsprechend des sBuffer aus  
+ **/
+  bool Shell::_execCommand() {
+    Serial.print("# ");
+    Serial.println(sBuffer);
+
+    if (sBuffer.equals("free")) {
+      _free();
+      return;
+    } else if (sBuffer.equals("help")) {
+      _help();
+      return;
+    } else if (sBuffer.equals("ls")) {
+      _ls();
+      return;
+    } else if (sBuffer.equals("sensors")) {
+      _sensors();
+      return;
+    } else {
+      Serial.println("> Error: unbekannter Befehl.");
+      _help();
+      return;
+    }
+  }
+
+
+
+  /**
+ * "help" zeigt verfügbare Kommandos an
+ */
+  void Shell::_help() {
+    Serial.println(F("> Verfügbare Kommandos: free, ls, help"));
+  }
+
+
+
+  /** 
+ * Kommando "free" gibt den freiem dynamischem Speicher (Heap) auf der Seriellen Konsole aus
+ */
+  void Shell::_free() {
+    extern int __heap_start, *__brkval;
+    int v;
+    Serial.print("> ");
+    Serial.print((int)&v - (__brkval == 0 ? (int)&__heap_start : (int)__brkval));
+    Serial.println(" Bytes free");
+  }
+
+
+
+  /**
+ * "ls" listet das Inhaltsverzeichnis der SD Karte auf
+ */
+  void Shell::_ls() {
+    sd.ls(LS_R);
+  }
+
+
+  void Shell::_sensors() {
+    Serial.println(s.getCsvSensorData());
+  }
 };
 #endif
